3090633300a3df49eea1096b40006d5fd76aa4f2
Gilles Sawa <gilles.sawa@spiratec.com>
2025-11-18 01:39:55 +0100

    adjust models

--- TicketSystem.Domain/Models/User.cs
+++ TicketSystem.Domain/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace TicketManagementSystem.Domain.Models;
 
 public class User
 {
+    [Key]
     public int Id { get; set; }
-    public string UserName { get; set; } = string.Empty;
-    public string Passwort { get; set; } = string.Empty;
-    public bool Useronnected { get; set; }
+    public string Vorname { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public bool UserConnected { get; set; }
 }
