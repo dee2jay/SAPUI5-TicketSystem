97a5f01b139a63c393faa364c086d67166a3a074
Gilles Sawa <gilles.sawa@spiratec.com>
2025-11-19 19:22:30 +0100

    remove unused events, commands and handlers

--- TicketManagement.Application/Command/RegisterUserCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TicketManagementSystem.Application.Commands;
-
-public record RegisterUserCommand(string Vorname, string Nachname, string Username, string Email, string Password);
